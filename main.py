11e9a5b3087c9aa099ff757ee0102638bb332f2b
Yara ElSakka <95961383+Yara-ElSakka@users.noreply.github.com>
2022-06-10 14:52:51 +0400

    minor changes

--- main.py
+++ main.py
@@ -7,12 +7,12 @@
 #    print(data)
 
 start_num = 0
-end_num = 100000
+end_num = 10
 sum = 0
 for n in range(start_num,end_num+1):
     sum = sum + int(n)
     #print(sum)
-print("SUM of first ", n, "numbers is: ", sum )
+print("SUM of first ", end_num, "numbers is: ", sum )
 
 
 # end of code
